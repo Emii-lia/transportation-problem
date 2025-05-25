e26c27e4b1ee2e12d5d72e9ab6620bf86dadf33e
Emii-lia <fiaromiangaly@gmail.com>
2025-05-25 08:13:22 +0300

    feat(transport-table): integrating transport table component

---
+++ src/components/TransportTable/index.tsx
@@ -0,0 +1,76 @@
+"use client"
+import CellInput from "@/components/CellInput";
+import "./TransportTable.scss"
+
+type Props = {
+  demands: number[]
+  supplies: number[]
+  costs: number[][],
+  onCellChange?: (rowIndex: number, colIndex: number, value: number) => void
+  onSupplyChange?: (index: number, value: number) => void
+  onDemandChange?: (index: number, value: number) => void
+  disabled?: boolean
+}
+const TransportTable = ({
+  costs,
+  demands,
+  disabled,
+  supplies,
+  onCellChange,
+  onSupplyChange,
+  onDemandChange
+}: Props) => {
+  return (
+    <div className="TransportTable">
+      <div
+        className="tr-row"
+      >
+        {costs.map((cRow, rowIndex) => (
+          <div
+            className="tr-col"
+            key={rowIndex}
+          >
+            {cRow.map((c, colIndex) => (
+              <CellInput
+                value={c}
+                key={colIndex}
+                onChange={
+                  !!onCellChange? (value: number) =>
+                    onCellChange(rowIndex, colIndex, value)
+                    : undefined
+                }
+                disabled={disabled}
+                withBorder
+              />
+            ))}
+            <CellInput
+              value={supplies[rowIndex]}
+              onChange={
+                !!onSupplyChange? (value: number) =>
+                  onSupplyChange(rowIndex, value)
+                  : undefined
+              }
+              disabled={disabled}
+            />
+          </div>
+        ))}
+        <div className="tr-col">
+          {demands.map((d, colIndex) => (
+            <CellInput
+              value={d}
+              key={colIndex}
+              onChange={
+                !!onDemandChange? (value: number) =>
+                  onDemandChange(colIndex, value)
+                  : undefined
+              }
+              disabled={disabled}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TransportTable
